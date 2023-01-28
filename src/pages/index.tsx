0ff334f578dcf183c62ee758901a38a523a10387
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-01-28 13:05:25 +0530

    fix: input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,38 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
-import type { NextPage } from 'next';
-import Navbar from '../components/navbar';
+import type { NextPage } from 'next'
+import Navbar from '../components/navbar'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Blog stats</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
-      <div className='block px-8  py-6 rounded-lg shadow-lg bg-white max-w-md mx-auto mt-2'>
+      <div className="block px-8  py-6 rounded-lg shadow-lg bg-white max-w-md mx-auto mt-2">
         <form>
-          <div className='form-group mb-6'>
+          <div className="form-group mb-6">
             <input
-              type='username'
-              className='form-control block w-full px-3 py-1.5 text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
-              aria-describedby='username for dev.to'
-              placeholder='Enter dev.to username'
+              type="username"
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 border border-solid border-gray-300 rounded m-0"
+              aria-describedby="username for dev.to"
+              placeholder="Enter dev.to username"
             />
           </div>
 
           <button
-            type='submit'
-            className='w-full rounded-lg text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium text-sm px-5 py-2.5 text-center mb-2'
+            type="submit"
+            className="w-full rounded-lg text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium text-sm px-5 py-2.5 text-center mb-2"
           >
             Submit
           </button>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
