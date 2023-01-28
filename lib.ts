1318025aecc38c81745298677f21013724066bc0
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-01-28 16:46:48 +0530

    fix: adding a new pre-commit hook

--- lib.ts
+++ lib.ts
@@ -1,9 +1,9 @@
 export const getYears = () => {
-    const endYear = new Date().getFullYear()
-    const years = []
-    let startYear = new Date().getFullYear() - 10
-    while (startYear <= endYear) {
-        years.push(startYear++)
-    }
-    return years
+  const endYear = new Date().getFullYear()
+  const years = []
+  let startYear = new Date().getFullYear() - 10
+  while (startYear <= endYear) {
+    years.push(startYear++)
+  }
+  return years
 }
--- src/pages/api/devYears.ts
+++ src/pages/api/devYears.ts
@@ -1,43 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from "axios";
+import axios from 'axios'
 
-import type { NextApiRequest, NextApiResponse } from "next";
-
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    const username = req.query.username;
-    const params = { username, per_page: 1000 };
-    const headers = { "api-key": process.env.DEV_API_KEY as string };
+  const username = req.query.username
+  const params = { username, per_page: 1000 }
+  const headers = { 'api-key': process.env.DEV_API_KEY as string }
 
-    try {
-        const articlesResponse = await axios.get(`https://dev.to/api/articles`, {
-            params,
-            headers,
-        });
+  try {
+    const articlesResponse = await axios.get(`https://dev.to/api/articles`, {
+      params,
+      headers,
+    })
 
-        const data = articlesResponse.data;
+    const data = articlesResponse.data
 
+    if (data.length !== 0) {
+      const years = data.map((d: { published_at: string | number | Date }) => {
+        return new Date(d.published_at).getFullYear()
+      })
 
-        if (data.length !== 0) {
-            const years = data.map((d: { published_at: string | number | Date; }) => {
-                return new Date(d.published_at).getFullYear()
-            })
+      const filtered = years.filter(
+        (y: number, index: number) => years.indexOf(y) === index
+      )
 
+      console.log(filtered)
 
-            const filtered = years.filter((y: number, index: number) => years.indexOf(y) === index)
-
-            console.log(filtered)
-
-            res.status(200).json(filtered);
-        }
-        else {
-            res.status(204).json(data)
-        }
-
-    } catch (e) {
-        console.error("something went wrong");
+      res.status(200).json(filtered)
+    } else {
+      res.status(204).json(data)
     }
+  } catch (e) {
+    console.error('something went wrong')
+  }
 }
